2792684733ee4a05f4de589d9896e5e240c0b439
Ritesh Maurya <riteshkumar411552@gmail.com>
2024-09-21 11:39:25 +0530

    initial query data

--- react-query/src/hooks/useSuperHeroData.js
+++ react-query/src/hooks/useSuperHeroData.js
@@ -1,12 +1,23 @@
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 const fetchSuperHero = ({ queryKey }) => {
   const heroId = queryKey[1];
-  console.log(heroId);
   return axios.get(`http://localhost:4000/superHeros/${heroId}`);
 };
 
 export const useSuperHeroData = (heroId) => {
-  return useQuery(["super-hero", heroId], fetchSuperHero);
+  const queryClient = new useQueryClient();
+  return useQuery(["super-hero", heroId], fetchSuperHero, {
+    initialData: () => {
+      const hero = queryClient
+        .getQueryData()
+        ?.data?.find((hero) => hero.id === parseInt(heroId));
+      if (hero) {
+        return { data: hero };
+      } else {
+        return undefined;
+      }
+    },
+  });
 };
